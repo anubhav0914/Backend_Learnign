4633bd13d83092810687cd56c1b534f6e8c8d9c9
anubhav0914 <anubhavg.cse22@gmail.com>
2025-03-01 01:24:46 +0530

    add user and video midel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 }
 )
-connectDB();
+
+
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log(`error`,err);
+        throw err
+    })
+    app.listen(process.env.PORT || 8001,()=>{
+        console.log(`Server is running on the port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`databse connection failed`,err)
+})
 
 
 
